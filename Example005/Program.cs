f9a7c9c8eed39467ce7aa78cd58d9a61ec1ecef4
kuninja <ilya.stasevich.98@gmail.com>
2023-06-09 14:14:05 +0300

    Случайные цифры, на примере сложения

---
+++ Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿//new Random().Next(min,max),[min, max-1] то есть если мне нужно число от 1 до 10, запись будет выглядеть так [1,9]\
+int numberA = new Random().Next(1,10);
+Console.Write("Первая цифра = ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("Вторая цифра = ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Сумма = ");
+Console.WriteLine(result);

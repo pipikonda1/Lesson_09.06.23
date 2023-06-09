4d4dd982ab387c51a45b0445eabf635c21d67f7c
kuninja <ilya.stasevich.98@gmail.com>
2023-06-09 14:27:23 +0300

    Гирьки

--- Example004_DIV/Program.cs
+++ Example004_DIV/Program.cs
@@ -1,8 +1,8 @@
-﻿int numberA = 12;
+﻿int numberA = 12; //int - целый тип данных
 int numberB = 5;
 int result1 = numberA / numberB;
 Console.WriteLine(result1);
-double numberC = 12;
-double numberD = 5;
+double numberC = 12; //double вещественный тип данных
+double numberD = 5;//Так же есть такой тип данных как  string - строки, и  bool - true or false
 double result2 = numberC / numberD;
 Console.WriteLine(result2);
--- Example006_IfElse/Program.cs
+++ Example006_IfElse/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.WriteLine("Введите своё имя: ");//WriteLine - в конце перейти на новую строку
 string username = Console.ReadLine();//ReadLine - считать строку из терминала
+
+if (username.ToLower == "Киса")
+{
+    Console.WriteLine("Доброго времени суток, кис)");
+}
+else
+{
 Console.Write("Привет, "); //Write - вывод в одну строку
 Console.WriteLine(username);
+}

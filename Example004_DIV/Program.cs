ed77402150be5b2a11a169b297d557b9d59f451b
kuninja <ilya.stasevich.98@gmail.com>
2023-06-09 13:39:44 +0300

    Деление чисел

---
+++ Example004_DIV/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberA = 12;
+int numberB = 5;
+int result1 = numberA / numberB;
+Console.WriteLine(result1);
+double numberC = 12;
+double numberD = 5;
+double result2 = numberC / numberD;
+Console.WriteLine(result2);
